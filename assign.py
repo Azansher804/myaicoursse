2e8a15e6e968ebf02c6d2c4315962264f35a6f96
Azansher804 <aazan0513@gmail.com>
2025-08-17 11:55:00 +0500

    Assignment

---
+++ assign.py
@@ -0,0 +1,37 @@
+list=[11, 'azan', 30.07, 'pakistan', 'Japan', ]
+print(list)
+print(list[1])
+  #assignment no#2
+  #for loop
+for country in list:
+    print(country)
+  #append
+list.append('123')
+print(list)
+  #assignment#3
+list.insert(0,'Toyottttta')  
+print(list)
+
+  #assignmetn#4
+  #remove item
+list.remove(30.07)
+print(list)
+  #popped method
+list.pop(0)
+print(list)
+  #replace item
+list[1]=500
+print(list)
+  #2
+list[2]='pak2'
+print(list)
+
+   #List type
+print(type(list))
+print(list)
+   #singleitem
+print(type(1))
+print(list)
+  
+
+
